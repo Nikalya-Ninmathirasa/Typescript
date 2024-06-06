2c23af437e122dd59864711bac38892b7d94d76a
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-06-06 14:06:31 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+let sales = 123_456_789;
+let course  = "ts"; // double or single
+let is_published: boolean = true;
+let level;
+
+
+function render(document:Document){
+    console.log(document)
+}
+
+let numbers = [];
+
+
+
